12956fa0594c7bc56ce77ddd556cf9e01dbceb77
abarhub <abarhub@gmail.com>
2026-02-08 09:56:35 +0100

    mise en place des sources

---
+++ pureboard-web/src/main/java/org/pureboard/pureboard/PureboardApplication.java
@@ -0,0 +1,13 @@
+package org.pureboard.pureboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PureboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PureboardApplication.class, args);
+	}
+
+}
---
+++ pureboard-web/src/test/java/org/pureboard/pureboard/PureboardApplicationTests.java
@@ -0,0 +1,13 @@
+package org.pureboard.pureboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PureboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
