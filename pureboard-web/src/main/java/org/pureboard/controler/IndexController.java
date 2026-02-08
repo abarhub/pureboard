45f809ec2243a361d88cf7645a12ff06ee73dc1e
abarhub <abarhub@gmail.com>
2026-02-08 21:57:38 +0100

    intégration d'angular dans spring boot

---
+++ pureboard-web/src/main/java/org/pureboard/controler/IndexController.java
@@ -0,0 +1,14 @@
+package org.pureboard.controler;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+//@Controller
+public class IndexController {
+
+//    @RequestMapping(value = "{path:[^\\.]*}")
+//    public String redirectIndexHtml() {
+//        return "forward:/index.html";
+//    }
+
+}
