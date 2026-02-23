8da973c5d7e364e0ee773b5b3bed7de59e1ce268
abarhub <abarhub@gmail.com>
2026-02-23 16:54:00 +0100

    utilisation de jquery comme dependance de du backend

--- pureboard-jquery/src/entity/contenu-dto.ts
+++ pureboard-jquery/src/entity/contenu-dto.ts
@@ -3,7 +3,7 @@ import {TableauDto} from './tableau-dto';
 
 export class ContenuDto {
 
-  type: TypeContenuDto = TypeContenuDto.INCONNU;
+  type: string = TypeContenuDto.INCONNU;
   texte: string = '';
   tableau: TableauDto | null = null;
 
--- pureboard-jquery/src/entity/type-contenu-dto.ts
+++ pureboard-jquery/src/entity/type-contenu-dto.ts
@@ -1,9 +1,9 @@
-export enum TypeContenuDto {
-  INCONNU = '',
-  TEXTE = 'TEXTE',
-  TABLEAU = 'TABLEAU',
-  ICONE = "ICONE",
-  COMPOSE = 'COMPOSE',
-  LIEN = 'LIEN',
-  BOUTON = 'BOUTON'
-}
+export const TypeContenuDto = {
+    INCONNU: '',
+    TEXTE: 'TEXTE',
+    TABLEAU: 'TABLEAU',
+    ICONE: "ICONE",
+    COMPOSE: 'COMPOSE',
+    LIEN: 'LIEN',
+    BOUTON: 'BOUTON'
+} as const;
